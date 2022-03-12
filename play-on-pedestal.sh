90d7fda0d70ee309729049f82129d1a74813ede4
thewebmasterp <thewebmsaterp@gmail.com>
2022-03-12 23:07:25 +0200

    Smoother volume transition added

--- play-on-pedestal.sh
+++ play-on-pedestal.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-function increase_sound {
+function increase_volume() {
   pactl list short sink-inputs | while read PID dump
   do
     VolumeForPID="$(/usr/bin/pacmd info | grep -A 5 $PID | grep -o '[^ ]*%' | grep -m 1 -o '[0-9]*')"
     if [ "$VolumeForPID" -lt "70" ]; then
-      # echo "increase volume by 29"
-      pactl -- set-sink-input-volume $PID +29%
+      # echo "increase volume by 30"
+      for run in {1..6}; do /usr/bin/pactl -- set-sink-input-volume $PID +5%; sleep 0.01; done
     fi
   done
 }
 
-function decrease_sound {
+function decrease_volume() {
   pactl list short sink-inputs | while read PID dump
   do
     VolumeForPID="$(/usr/bin/pacmd info | grep -A 5 $PID | grep -o '[^ ]*%' | grep -m 1 -o '[0-9]*')"
     if [ "$VolumeForPID" -gt "30" ]; then
-      # echo "decrease volume by 29"
-      pactl -- set-sink-input-volume $PID -29%
+      # echo "decrease volume by 30"
+      for run in {1..6}; do /usr/bin/pactl -- set-sink-input-volume $PID -5%; done
     fi
   done
 }
 
-decrease_sound
-sleep 0.1
+decrease_volume
+sleep 0.01
 /usr/bin/paplay --volume 65536 $1
-sleep 0.1
-increase_sound
+sleep 0.01
+increase_volume
 
